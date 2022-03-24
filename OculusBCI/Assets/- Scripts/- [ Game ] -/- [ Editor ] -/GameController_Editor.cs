b2946f2305b4437126293290ffdc187841dca9fc
AgentArachnid66 <paulbrown.prb66@gmail.com>
2022-03-24 19:22:58 +0000

    changes

---
+++ OculusBCI/Assets/- Scripts/- [ Game ] -/- [ Editor ] -/GameController_Editor.cs
@@ -0,0 +1,26 @@
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(GameController))]
+public class GameController_Editor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        EditorGUILayout.LabelField("Game Controller");
+        GameController targetScript = (GameController) target;
+
+        if (!Application.isPlaying)
+        {
+            base.OnInspectorGUI();
+        }
+        else
+        {
+            if (GUILayout.Button("Increment State"))
+            {
+                targetScript.gameState++;
+            }
+        }
+    }
+}
+#endif
